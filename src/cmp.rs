6e4e3592ba9bbb0be6c856bdd061390d44f655de
Toggy Smith <git@toggysmith.com>
2026-07-11 16:00:35 +0100

    refactor: run cargo fmt

--- src/cmp.rs
+++ src/cmp.rs
@@ -3,8 +3,8 @@
 // For the full copyright and license information, please view the LICENSE-*
 // files that was distributed with this source code.
 
-use crate::utils::format_failure_to_read_input_file;
 use crate::safe_println;
+use crate::utils::format_failure_to_read_input_file;
 use std::env::{self, ArgsOs};
 use std::ffi::OsString;
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
